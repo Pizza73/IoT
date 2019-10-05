b3bd998b7f4048f0358c9ad845c1ce7658ff0603
Pizza73 <archisthebest@yahoo.co.jp>
2019-10-05 11:29:59 +0900

    gpio test

--- gpio.py
+++ gpio.py
@@ -7,12 +7,14 @@ Created on Sat Oct  5 10:44:29 2019
 """
 
 import RPi.GPIO as GPIO
+import time
 
 def main():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(32,GPIO.IN)
     val=GPIO.input(32)
     print(val)
+    time.sleep(2)
     
     
 if __name__=='__main__':
