ab55d615998c288fdbbe408c09f1be57359e3e27
Pizza73 <archisthebest@yahoo.co.jp>
2020-01-10 08:10:40 +0900

    LINE push

---
+++ LINE_open.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl -v -X POST https://api.line.me/v2/bot/message/broadcast \
+-H 'Content-Type: application/json' \
+-H 'Authorization: Bearer {uuDcgrnY6jVi46SNbWFz2nCQMvNqEwyim/8z2DXMxAYOMv02M+kDG/o/pUUX2ufo1JqXIaDpa8F/DgK5FDVHSvuYqhcHAEYDxlNebW+LUp56rHUpN5YpH8HH9RqjGR7wq5Pc51PxZvwxKZbW61/BJAdB04t89/1O/w1cDnyilFU=}' \
+-d '{
+    "messages":[
+        {
+            "type":"text",
+            "text":"熱中症を発症するリスクが低くなったので、窓を開けてエアコンを消しました"
+        },
+        {
+            "type":"text",
+            "text":"部屋の状態\n温度：20℃\n湿度：50％"
+        }
+    ]
+}'
